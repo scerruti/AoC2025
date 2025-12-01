ddc9292b12bd12f5b2e01d26a0ffbe757b3f1632
Stephen Cerruti <scerruti@miracosta.edu>
2025-11-30 22:06:23 -0800

    feat: add Day 1 solution and sample input

--- src/main/java/aoc2025/solutions/Day01.java
+++ src/main/java/aoc2025/solutions/Day01.java
@@ -7,13 +7,63 @@ public class Day01 extends Day {
 
     @Override
     public String part1(List<String> input) {
-        // TODO: Implement Part 1
-        return "";
+        int position = 50;
+        int count = 0;
+        for (String line : input) {
+            int clicks = Integer.parseInt(line.substring(1));
+            if (line.substring(0,1).equals("L")) {
+                clicks = -clicks;   
+            }
+
+            position += clicks;
+            while (position < 0) {
+                position += 100;
+            } 
+            
+            while (position >= 100) {
+                position -= 100;
+            }
+            
+            if (position == 0) {
+                count++;
+            }
+
+        }
+        return Integer.toString(count);
     }
 
     @Override
     public String part2(List<String> input) {
-        // TODO: Implement Part 2
-        return "";
-    }
+        int position = 50;
+        int count = 0;
+        for (String line : input) {
+            int clicks = Integer.parseInt(line.substring(1));
+            count += clicks / 100;
+            clicks = clicks % 100;
+            if (line.substring(0,1).equals("L")) {
+                clicks = -clicks;   
+            }
+
+            int oldPosition = position;
+            position += clicks;
+            System.out.println("After move of: " + clicks + " position: " + position);
+            if (position < 0 ) {
+                position += 100;
+                if (oldPosition > 0) count++;
+                System.out.println("Wrapped up to: " + position);
+            } else if (position >= 100) {
+                position -= 100;
+                count++;
+                System.out.println("Wrapped down to: " + position);
+            } else if (position > 0 && oldPosition < 0) {
+                count++;
+                System.out.println("Wrapped up to: " + position);
+            } else if (position == 0) {
+                count++;
+            }
+
+            System.out.println("Current position: " + position + " count: " + count);
+
+        }
+        return Integer.toString(count);    }
 }
