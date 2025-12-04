5401753fe1b7d333da4074da3f8f4f818e5ae30c
Stephen Cerruti <scerruti@miracosta.edu>
2025-12-03 22:05:54 -0800

    Add Day 4: Warehouse writeup with debugging sections and fix paper roll description

--- src/main/java/aoc2025/solutions/Day04.java
+++ src/main/java/aoc2025/solutions/Day04.java
@@ -8,13 +8,69 @@ public class Day04 extends Day {
 
     @Override
     public String part1(ArrayList<String> input) {
-        // TODO: Implement Part 1
-        return "";
+        int count = 0;
+        int width = input.get(0).length();
+
+        int[][] warehouse = getRowNumber(input, width);
+
+        for (int rowNumber = 1; rowNumber <= width; rowNumber++) {
+            for (int colNumber = 1; colNumber <= width; colNumber++) {
+                if (warehouse[rowNumber][colNumber] == 1 && numberNeighbors(warehouse, rowNumber, colNumber) < 4) {
+                    count += 1;
+                }
+            }
+        }
+        return String.valueOf(count);
+    }
+
+    private int[][] getRowNumber(ArrayList<String> input, int width) {
+        int[][] warehouse = new int[width + 2][width + 2];
+        int rowNumber = 1;
+        for (String row : input) {
+            for (int colNumber = 1; colNumber <= width; colNumber++) {
+                if (row.substring(colNumber-1, colNumber).equals("@")) {
+                    warehouse[rowNumber][colNumber] = 1;
+                }
+            }
+            rowNumber += 1;
+        }
+
+        return warehouse;
+    }
+
+    private int numberNeighbors(int[][] warehouse, int rowNumber, int colNumber) {
+        return warehouse[rowNumber-1][colNumber-1] + warehouse[rowNumber-1][colNumber] + warehouse[rowNumber-1][colNumber+1] +
+               warehouse[rowNumber][colNumber-1] + warehouse[rowNumber][colNumber+1] +
+               warehouse[rowNumber+1][colNumber-1] + warehouse[rowNumber+1][colNumber] + warehouse[rowNumber+1][colNumber+1];
     }
 
     @Override
     public String part2(ArrayList<String> input) {
-        // TODO: Implement Part 2
-        return "";
+        int count = 0;
+        int width = input.get(0).length();
+
+        int[][] warehouse = getRowNumber(input, width);
+
+        boolean rollsRemoved = true;
+        while (rollsRemoved) {
+            rollsRemoved = false;
+            for (int rowNumber = 1; rowNumber <= width; rowNumber++) {
+                for (int colNumber = 1; colNumber <= width; colNumber++) {
+                    if (warehouse[rowNumber][colNumber] == 1 && numberNeighbors(warehouse, rowNumber, colNumber) < 4) {
+                        count += 1;
+                        warehouse[rowNumber][colNumber] = 2;
+                    }
+                }
+            }
+            for (int rowNumber = 1; rowNumber <= width; rowNumber++) {
+                for (int colNumber = 1; colNumber <= width; colNumber++) {
+                    if (warehouse[rowNumber][colNumber] == 2) {
+                        warehouse[rowNumber][colNumber] = 0;
+                        rollsRemoved = true;
+                    }
+                }
+            }
+        }
+        return String.valueOf(count);
     }
 }
