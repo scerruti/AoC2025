df6028c42bf03bcf2127b05e4e8b3a0c806b292e
Stephen Cerruti <scerruti@miracosta.edu>
2025-12-07 09:56:05 -0800

    Day 7: Initial solution

--- src/main/java/aoc2025/solutions/Day07.java
+++ src/main/java/aoc2025/solutions/Day07.java
@@ -1,20 +1,162 @@
 package aoc2025.solutions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 
 public class Day07 extends Day {
+    private static final int EMPTY = '.';
+    private static final int START = 'S';
+    private static final int SPLITTER = '^';
     public Day07() { super(7); }
 
     @Override
     public String part1(ArrayList<String> input) {
-        // TODO: Implement Part 1
-        return "";
+        int tachyonFieldWidth = input.get(0).length();
+        int tachyonFieldLength = input.size();
+        int[][] tachyonField = new int[tachyonFieldLength][tachyonFieldWidth];
+        int start = input.get(0).indexOf("S");
+
+        for (int row = 0; row < tachyonFieldLength; row++) {
+            for (int col = 0; col < tachyonFieldWidth; col++) {
+                tachyonField[row][col] = input.get(row).charAt(col);
+            }
+        }
+
+        int row = 1;
+        int splitCount = 0;
+        ArrayList<Integer> beamColumns;
+        ArrayList<Integer> nextBeamColumns = new ArrayList<>();
+        nextBeamColumns.add(new Integer(start));
+
+        while (row < tachyonFieldLength - 1) {
+            row += 1;
+            beamColumns = nextBeamColumns;
+            nextBeamColumns = new ArrayList<>();
+
+            for (Integer column : beamColumns) {
+                if (tachyonField[row][column] == EMPTY) {
+                    addIfUnique(nextBeamColumns,column);
+                } else if (tachyonField[row][column] == SPLITTER) {
+                    addIfUnique(nextBeamColumns, column - 1);
+                    addIfUnique(nextBeamColumns, column + 1);
+                    splitCount += 1;
+                }
+            }
+        }
+
+        return String.valueOf(splitCount);
+    }
+
+    private boolean addIfUnique(ArrayList<Integer> nextBeamColumns, Integer column) {
+        if (nextBeamColumns.contains(column)) return false;
+
+        nextBeamColumns.add(column);
+        return true;
     }
 
     @Override
     public String part2(ArrayList<String> input) {
-        // TODO: Implement Part 2
-        return "";
+        int tachyonFieldWidth = input.get(0).length();
+        int tachyonFieldLength = input.size();
+        int[][] tachyonField = new int[tachyonFieldLength][tachyonFieldWidth];
+        int start = input.get(0).indexOf("S");
+
+        for (int row = 0; row < tachyonFieldLength; row++) {
+            for (int col = 0; col < tachyonFieldWidth; col++) {
+                tachyonField[row][col] = input.get(row).charAt(col);
+            }
+        }
+
+        int pathCount = 0;
+        ArrayList<Point> particleLocations = new ArrayList<>();
+        HashMap<Point, Integer> cache = new HashMap<>();
+        particleLocations.add(new Point(0, start));
+
+        while (particleLocations.size() > 0) {
+            Point current = particleLocations.get(0);
+            // System.out.println(current);
+            Point next = new Point(current.row+1, current.column);
+            Point right = new Point(current.row+1, current.column + 1);
+            Point left = new Point(current.row+1, current.column - 1);
+
+            if (cache.keySet().contains(current)) {
+                pathCount = cache.get(current);
+                cache.put(current, pathCount * 2);
+                // System.out.println("Cached: " + current + " " +cache.get(current));
+                particleLocations.remove(0);
+                continue;
+            }
+
+            int nextRow = current.row + 1;
+            if (nextRow >= tachyonFieldLength) {
+                cache.put(current, 1);
+                // System.out.println("Cached: " + current + " " +cache.get(current));
+                particleLocations.remove(0);
+                continue;
+            }
+
+            if (tachyonField[nextRow][current.column] == EMPTY) {
+                if (cache.keySet().contains(next)) {
+                    cache.put(current, cache.get(next));
+                    // System.out.println("Cached: " + current + " " +cache.get(current));
+                   particleLocations.remove(0);
+                } else {
+                    particleLocations.add(0, next);
+                }
+            } else if (tachyonField[nextRow][current.column] == SPLITTER) {
+                boolean descended = false;
+                int lowerPaths = 0;
+                if (cache.keySet().contains(right)) {
+                    lowerPaths = cache.get(right);
+                } else {
+                    particleLocations.add(0, right);
+                    descended = true;
+                }
+                if (cache.keySet().contains(left)) {
+                    lowerPaths += cache.get(left);
+                } else {
+                    particleLocations.add(0, left);
+                    descended = true;
+                }
+                if (!descended) {
+                    cache.put(current, lowerPaths);
+                    // System.out.println("Cached: " + current + " " +cache.get(current));
+                    particleLocations.remove(0);
+                }
+            }
+        }
+
+        return String.valueOf(cache.get(new Point(0, start)));    
+    }
+
+    public class Point {
+        Integer row;
+        Integer column;
+
+        public Point(Integer row, Integer column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        @Override
+        public String toString() {
+            return "Point [row=" + row + ", column=" + column + "]";
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false; // Or use instanceof
+            Point point = (Point) o;
+
+            return row == point.row && column == point.column;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, column);
+        }
     }
 }
